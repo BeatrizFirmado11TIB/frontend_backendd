4129d9455d0ca2f3e6a53bd9b143873c02413085
Beatriz Firmado <firmadobeatriz@gmail.com>
2025-10-23 00:12:34 -0300

    projetoo

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+import express, { request, response } from "express"
+import cors from "cors"
+import {persons} from "./person.js"
+
+const app = express ()
+const port = 3333
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (request, response) =>{
+    response.json(persons)
+})
+
+app.post("/cadastrar", (request, response) =>{
+    const {user} = request.body
+    console.log (user)
+    response.status(201).json({message: "Usuário cadastrado com sucesso!"})
+})
+
+app.listen(port, () =>{
+    console.log(`Server running on port ${port}!`)
+})
